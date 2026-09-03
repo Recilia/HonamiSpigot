3ffe64f521edc5754e2d2a4ef068963bc0637adc
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:29:43 +0300

    Add Honami-Server/src/main/java/rein/honami/spigot/async/entitytracker/AsyncEntityTracker.java

---
+++ Honami-Server/src/main/java/rein/honami/spigot/async/entitytracker/AsyncEntityTracker.java
@@ -0,0 +1,53 @@
+package rein.honami.spigot.async.entitytracker;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import rein.honami.spigot.async.AsyncUtil;
+import rein.honami.spigot.config.HonamiConfig;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import rein.honami.spigot.playermap.utils.IndexedLinkedHashSet;
+import net.minecraft.server.*;
+
+public class AsyncEntityTracker extends EntityTracker {
+	
+	private static final ExecutorService trackingThreadExecutor = Executors.newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat("Honami Entity Tracker Thread").build());
+	private final WorldServer worldServer;	
+	
+	public AsyncEntityTracker(WorldServer worldserver) {
+		super(worldserver);
+		this.worldServer = worldserver;
+	}
+	
+	@Override
+	public void updatePlayers() {	
+		int offset = 0;
+		
+		for (int i = 1; i <= HonamiConfig.trackingThreads; i++) {
+			final int finalOffset = offset++;
+			
+			AsyncUtil.run(() -> {
+				for (int index = finalOffset; index < c.size(); index += HonamiConfig.trackingThreads) {
+                    ((IndexedLinkedHashSet<EntityTrackerEntry>) c).get(index).update();
+				}
+				worldServer.ticker.getLatch().decrement();
+
+			}, trackingThreadExecutor);
+			
+		}
+		try {
+            worldServer.ticker.getLatch().waitTillZero();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+	    worldServer.ticker.getLatch().reset();
+		for (EntityPlayer player : MinecraftServer.getServer().getPlayerList().players) {
+			player.playerConnection.sendQueuedPackets();
+		}
+	}
+
+	public static ExecutorService getExecutor() {
+		return trackingThreadExecutor;
+	}
+}
