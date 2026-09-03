00942e278d39ee2820f9349a09575627052d2ba3
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:08:12 +0300

    Add Honami-API/src/main/java/org/bukkit/event/painting/PaintingEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/painting/PaintingEvent.java
@@ -0,0 +1,19 @@
+package org.bukkit.event.painting;
+
+import org.bukkit.Warning;
+import org.bukkit.entity.Painting;
+import org.bukkit.event.Event;
+
+@Deprecated
+@Warning(reason = "This event has been replaced by HangingEvent")
+public abstract class PaintingEvent extends Event {
+	protected Painting painting;
+
+	protected PaintingEvent(final Painting painting) {
+		this.painting = painting;
+	}
+
+	public Painting getPainting() {
+		return painting;
+	}
+}
