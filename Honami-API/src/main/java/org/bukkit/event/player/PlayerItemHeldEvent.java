3631cb27ae0fe0897f100ec13332702c06ec3264
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:09:37 +0300

    Add Honami-API/src/main/java/org/bukkit/event/player/PlayerItemHeldEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/player/PlayerItemHeldEvent.java
@@ -0,0 +1,43 @@
+package org.bukkit.event.player;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class PlayerItemHeldEvent extends PlayerEvent implements Cancellable {
+	private static final HandlerList handlers = new HandlerList();
+	private boolean cancel = false;
+	private final int previous;
+	private final int current;
+
+	public PlayerItemHeldEvent(final Player player, final int previous, final int current) {
+		super(player);
+		this.previous = previous;
+		this.current = current;
+	}
+
+	public int getPreviousSlot() {
+		return previous;
+	}
+
+	public int getNewSlot() {
+		return current;
+	}
+
+	public boolean isCancelled() {
+		return cancel;
+	}
+
+	public void setCancelled(boolean cancel) {
+		this.cancel = cancel;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
