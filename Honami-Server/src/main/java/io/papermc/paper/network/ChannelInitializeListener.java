4ee2fd889d7e93082c792ae4f54e46be52777f25
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:34:24 +0300

    Add io/papermc/paper/network/ChannelInitializeListener.java

---
+++ Honami-Server/src/main/java/io/papermc/paper/network/ChannelInitializeListener.java
@@ -0,0 +1,10 @@
+package io.papermc.paper.network;
+
+import io.netty.channel.Channel;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+@FunctionalInterface
+public interface ChannelInitializeListener {
+
+    void afterInitChannel(@NonNull Channel channel);
+}
