e1defbd365c6042a62f36814aa1c5099278d028a
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:24:54 +0300

    Add Honami-Server/src/main/java/org/bukkit/craftbukkit/generator/InternalChunkGenerator.java

---
+++ Honami-Server/src/main/java/org/bukkit/craftbukkit/generator/InternalChunkGenerator.java
@@ -0,0 +1,8 @@
+package org.bukkit.craftbukkit.generator;
+
+import org.bukkit.generator.ChunkGenerator;
+
+import net.minecraft.server.IChunkProvider;
+
+public abstract class InternalChunkGenerator extends ChunkGenerator implements IChunkProvider {
+}
