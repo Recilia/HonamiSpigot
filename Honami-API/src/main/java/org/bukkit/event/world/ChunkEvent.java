8aa011abf1501dcc8f77bb214e87fbc6973e1ab2
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:11:44 +0300

    Add Honami-API/src/main/java/org/bukkit/event/world/ChunkEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/world/ChunkEvent.java
@@ -0,0 +1,16 @@
+package org.bukkit.event.world;
+
+import org.bukkit.Chunk;
+
+public abstract class ChunkEvent extends WorldEvent {
+	protected Chunk chunk;
+
+	protected ChunkEvent(final Chunk chunk) {
+		super(chunk.getWorld());
+		this.chunk = chunk;
+	}
+
+	public Chunk getChunk() {
+		return chunk;
+	}
+}
