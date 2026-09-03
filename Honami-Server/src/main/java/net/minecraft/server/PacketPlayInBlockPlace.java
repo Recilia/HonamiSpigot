22f77c9fa587b5b817dd1851c1be316f42d8fe8c
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:45:19 +0300

    Add net/minecraft/server/PacketPlayInBlockPlace.java

---
+++ Honami-Server/src/main/java/net/minecraft/server/PacketPlayInBlockPlace.java
@@ -0,0 +1,83 @@
+package net.minecraft.server;
+
+import java.io.IOException;
+
+public class PacketPlayInBlockPlace implements Packet<PacketListenerPlayIn> {
+
+	private static final BlockPosition a = new BlockPosition(-1, -1, -1);
+	private BlockPosition b;
+	private int c;
+	private ItemStack d;
+	private float e;
+	private float f;
+	private float g;
+
+	public long timestamp; 
+
+	public PacketPlayInBlockPlace() {
+	}
+
+	public PacketPlayInBlockPlace(ItemStack itemstack) {
+		this(PacketPlayInBlockPlace.a, 255, itemstack, 0.0F, 0.0F, 0.0F);
+	}
+
+	public PacketPlayInBlockPlace(BlockPosition blockposition, int i, ItemStack itemstack, float f, float f1,
+			float f2) {
+		this.b = blockposition;
+		this.c = i;
+		this.d = itemstack != null ? itemstack.cloneItemStack() : null;
+		this.e = f;
+		this.f = f1;
+		this.g = f2;
+	}
+
+	@Override
+	public void a(PacketDataSerializer serializer) throws IOException {
+		timestamp = System.currentTimeMillis(); 
+		this.b = serializer.c();
+		this.c = serializer.readUnsignedByte();
+		this.d = serializer.decodeItemStack();
+		this.e = serializer.readUnsignedByte() / 16.0F;
+		this.f = serializer.readUnsignedByte() / 16.0F;
+		this.g = serializer.readUnsignedByte() / 16.0F;
+	}
+
+	@Override
+	public void b(PacketDataSerializer serializer) throws IOException {
+		serializer.a(this.b);
+		serializer.writeByte(this.c);
+		serializer.a(this.d);
+		serializer.writeByte((int) (this.e * 16.0F));
+		serializer.writeByte((int) (this.f * 16.0F));
+		serializer.writeByte((int) (this.g * 16.0F));
+	}
+
+	@Override
+	public void a(PacketListenerPlayIn packetlistenerplayin) {
+		packetlistenerplayin.a(this);
+	}
+
+	public BlockPosition a() {
+		return this.b;
+	}
+
+	public int getFace() {
+		return this.c;
+	}
+
+	public ItemStack getItemStack() {
+		return this.d;
+	}
+
+	public float d() {
+		return this.e;
+	}
+
+	public float e() {
+		return this.f;
+	}
+
+	public float f() {
+		return this.g;
+	}
+}
