6031eea531b1bb7028c41944d1d5be23c64e2a82
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:10:23 +0300

    Add Honami-API/src/main/java/org/bukkit/event/player/PlayerToggleSneakEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/player/PlayerToggleSneakEvent.java
@@ -0,0 +1,37 @@
+package org.bukkit.event.player;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class PlayerToggleSneakEvent extends PlayerEvent implements Cancellable {
+	private static final HandlerList handlers = new HandlerList();
+	private final boolean isSneaking;
+	private boolean cancel = false;
+
+	public PlayerToggleSneakEvent(final Player player, final boolean isSneaking) {
+		super(player);
+		this.isSneaking = isSneaking;
+	}
+
+	public boolean isSneaking() {
+		return isSneaking;
+	}
+
+	public boolean isCancelled() {
+		return cancel;
+	}
+
+	public void setCancelled(boolean cancel) {
+		this.cancel = cancel;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
