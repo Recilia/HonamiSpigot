89c0ee1db5782b94df683178b577feac08909b39
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:07:13 +0300

    Add Honami-API/src/main/java/org/bukkit/event/hanging/HangingEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/hanging/HangingEvent.java
@@ -0,0 +1,16 @@
+package org.bukkit.event.hanging;
+
+import org.bukkit.entity.Hanging;
+import org.bukkit.event.Event;
+
+public abstract class HangingEvent extends Event {
+	protected Hanging hanging;
+
+	protected HangingEvent(final Hanging painting) {
+		this.hanging = painting;
+	}
+
+	public Hanging getEntity() {
+		return hanging;
+	}
+}
