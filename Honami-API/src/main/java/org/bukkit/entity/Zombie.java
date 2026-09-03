b0445d6cb3beba88c27ac30de7be2d8b27f5ee94
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:03:17 +0300

    Add Honami-API/src/main/java/org/bukkit/entity/Zombie.java

---
+++ Honami-API/src/main/java/org/bukkit/entity/Zombie.java
@@ -0,0 +1,12 @@
+package org.bukkit.entity;
+
+public interface Zombie extends Monster {
+
+	public boolean isBaby();
+
+	public void setBaby(boolean flag);
+
+	public boolean isVillager();
+
+	public void setVillager(boolean flag);
+}
