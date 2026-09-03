4c9b32c9afb26ac103f209b80f08780be1963949
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:16:06 +0300

    Add Honami-API/src/main/java/org/bukkit/material/PressurePlate.java

---
+++ Honami-API/src/main/java/org/bukkit/material/PressurePlate.java
@@ -0,0 +1,42 @@
+package org.bukkit.material;
+
+import org.bukkit.Material;
+
+public class PressurePlate extends MaterialData implements PressureSensor {
+	public PressurePlate() {
+		super(Material.WOOD_PLATE);
+	}
+
+	@Deprecated
+	public PressurePlate(int type) {
+		super(type);
+	}
+
+	public PressurePlate(Material type) {
+		super(type);
+	}
+
+	@Deprecated
+	public PressurePlate(int type, byte data) {
+		super(type, data);
+	}
+
+	@Deprecated
+	public PressurePlate(Material type, byte data) {
+		super(type, data);
+	}
+
+	public boolean isPressed() {
+		return getData() == 0x1;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + (isPressed() ? " PRESSED" : "");
+	}
+
+	@Override
+	public PressurePlate clone() {
+		return (PressurePlate) super.clone();
+	}
+}
