0d637b6b305d0b17ce8c6f670d8e720fbcabd74a
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:39:41 +0300

    Add net/minecraft/server/EntityAgeable.java

---
+++ Honami-Server/src/main/java/net/minecraft/server/EntityAgeable.java
@@ -0,0 +1,199 @@
+package net.minecraft.server;
+
+import rein.honami.spigot.cache.Constants;
+
+public abstract class EntityAgeable extends EntityCreature {
+
+	protected int a;
+	protected int b;
+	protected int c;
+	private float bm = -1.0F;
+	private float bn;
+	public boolean ageLocked = false; 
+
+	@Override
+	public void inactiveTick() {
+		super.inactiveTick();
+		if (this.world.isClientSide || this.ageLocked) { 
+			this.a(this.isBaby());
+		} else {
+			int i = this.getAge();
+
+			if (i < 0) {
+				++i;
+				this.setAgeRaw(i);
+			} else if (i > 0) {
+				--i;
+				this.setAgeRaw(i);
+			}
+		}
+	}
+
+	public EntityAgeable(World world) {
+		super(world);
+	}
+
+	public abstract EntityAgeable createChild(EntityAgeable entityageable);
+
+	@Override
+	public boolean a(EntityHuman entityhuman) {
+		ItemStack itemstack = entityhuman.inventory.getItemInHand();
+
+		if (itemstack != null && itemstack.getItem() == Items.SPAWN_EGG) {
+			if (!this.world.isClientSide) {
+				Class oclass = EntityTypes.a(itemstack.getData());
+
+				if (oclass != null && this.getClass() == oclass) {
+					EntityAgeable entityageable = this.createChild(this);
+
+					if (entityageable != null) {
+						entityageable.setAgeRaw(-24000);
+						entityageable.setPositionRotation(this.locX, this.locY, this.locZ, 0.0F, 0.0F);
+						this.world.addEntity(entityageable,
+								org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason.SPAWNER_EGG); 
+						if (itemstack.hasName()) {
+							entityageable.setCustomName(itemstack.getName());
+						}
+
+						if (!entityhuman.abilities.canInstantlyBuild) {
+							--itemstack.count;
+							if (itemstack.count == 0) { 
+								entityhuman.inventory.setItem(entityhuman.inventory.itemInHandIndex, (ItemStack) null);
+							}
+						}
+					}
+				}
+			}
+
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	protected void h() {
+		super.h();
+		this.datawatcher.a(12, Byte.valueOf((byte) 0));
+	}
+
+	public int getAge() {
+		return this.world.isClientSide ? this.datawatcher.getByte(12) : this.a;
+	}
+
+	public void setAge(int i, boolean flag) {
+		int j = this.getAge();
+		int k = j;
+
+		j += i * 20;
+		if (j > 0) {
+			j = 0;
+			if (k < 0) {
+				this.n();
+			}
+		}
+
+		int l = j - k;
+
+		this.setAgeRaw(j);
+		if (flag) {
+			this.b += l;
+			if (this.c == 0) {
+				this.c = 40;
+			}
+		}
+
+		if (this.getAge() == 0) {
+			this.setAgeRaw(this.b);
+		}
+
+	}
+
+	public void setAge(int i) {
+		this.setAge(i, false);
+	}
+
+	public void setAgeRaw(int i) {
+		this.datawatcher.watch(12, Byte.valueOf((byte) MathHelper.clamp(i, -1, 1)));
+		this.a = i;
+		this.a(this.isBaby());
+	}
+
+	@Override
+	public void b(NBTTagCompound nbttagcompound) {
+		super.b(nbttagcompound);
+		nbttagcompound.setInt("Age", this.getAge());
+		nbttagcompound.setInt("ForcedAge", this.b);
+		nbttagcompound.setBoolean("AgeLocked", this.ageLocked); 
+	}
+
+	@Override
+	public void a(NBTTagCompound nbttagcompound) {
+		super.a(nbttagcompound);
+		this.setAgeRaw(nbttagcompound.getInt("Age"));
+		this.b = nbttagcompound.getInt("ForcedAge");
+		this.ageLocked = nbttagcompound.getBoolean("AgeLocked"); 
+	}
+
+	@Override
+	public void m() {
+		super.m();
+		if (this.world.isClientSide || ageLocked) { 
+			if (this.c > 0) {
+				if (this.c % 4 == 0) {
+					this.world.addParticle(EnumParticle.VILLAGER_HAPPY,
+							this.locX + this.random.nextFloat() * this.width * 2.0F - this.width,
+							this.locY + 0.5D + this.random.nextFloat() * this.length,
+							this.locZ + this.random.nextFloat() * this.width * 2.0F - this.width, 0.0D, 0.0D, 0.0D,
+							Constants.EMPTY_ARRAY);
+				}
+
+				--this.c;
+			}
+
+			this.a(this.isBaby());
+		} else {
+			int i = this.getAge();
+
+			if (i < 0) {
+				++i;
+				this.setAgeRaw(i);
+				if (i == 0) {
+					this.n();
+				}
+			} else if (i > 0) {
+				--i;
+				this.setAgeRaw(i);
+			}
+		}
+
+	}
+
+	protected void n() {
+	}
+
+	@Override
+	public boolean isBaby() {
+		return this.getAge() < 0;
+	}
+
+	public void a(boolean flag) {
+		this.a(flag ? 0.5F : 1.0F);
+	}
+
+	@Override
+	public final void setSize(float f, float f1) { 
+		boolean flag = this.bm > 0.0F;
+
+		this.bm = f;
+		this.bn = f1;
+		if (!flag) {
+			this.a(1.0F);
+		}
+
+	}
+
+	protected final void a(float f) {
+		super.setSize(this.bm * f, this.bn * f);
+	}
+}
