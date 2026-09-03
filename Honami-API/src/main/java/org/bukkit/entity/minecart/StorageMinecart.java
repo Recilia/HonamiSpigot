bc58cf008b77020365babae8102d898f1a636700
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:03:35 +0300

    Add Honami-API/src/main/java/org/bukkit/entity/minecart/StorageMinecart.java

---
+++ Honami-API/src/main/java/org/bukkit/entity/minecart/StorageMinecart.java
@@ -0,0 +1,7 @@
+package org.bukkit.entity.minecart;
+
+import org.bukkit.entity.Minecart;
+import org.bukkit.inventory.InventoryHolder;
+
+public interface StorageMinecart extends Minecart, InventoryHolder {
+}
