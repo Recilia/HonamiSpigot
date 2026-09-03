cf09dcdfbc4485b85541503d47194e004d3c2e05
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:10:26 +0300

    Add Honami-API/src/main/java/org/bukkit/event/player/PlayerToggleSprintEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/player/PlayerToggleSprintEvent.java
@@ -0,0 +1,37 @@
+package org.bukkit.event.player;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class PlayerToggleSprintEvent extends PlayerEvent implements Cancellable {
+	private static final HandlerList handlers = new HandlerList();
+	private final boolean isSprinting;
+	private boolean cancel = false;
+
+	public PlayerToggleSprintEvent(final Player player, final boolean isSprinting) {
+		super(player);
+		this.isSprinting = isSprinting;
+	}
+
+	public boolean isSprinting() {
+		return isSprinting;
+	}
+
+	public boolean isCancelled() {
+		return cancel;
+	}
+
+	public void setCancelled(boolean cancel) {
+		this.cancel = cancel;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
