84249d383f63f8a6e6a9668cc48853cf04eb6e21
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:05:19 +0300

    Add Honami-API/src/main/java/org/bukkit/event/entity/EntityBreakDoorEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/entity/EntityBreakDoorEvent.java
@@ -0,0 +1,17 @@
+package org.bukkit.event.entity;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+
+public class EntityBreakDoorEvent extends EntityChangeBlockEvent {
+	public EntityBreakDoorEvent(final LivingEntity entity, final Block targetBlock) {
+		super(entity, targetBlock, Material.AIR, (byte) 0);
+	}
+
+	@Override
+	public LivingEntity getEntity() {
+		return (LivingEntity) entity;
+	}
+}
