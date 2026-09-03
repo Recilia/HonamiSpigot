ece3a9ef28f54adb8aba4247f9d972c7c8c515aa
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:17:19 +0300

    Add Honami-API/src/main/java/org/bukkit/metadata/MetadataEvaluationException.java

---
+++ Honami-API/src/main/java/org/bukkit/metadata/MetadataEvaluationException.java
@@ -0,0 +1,8 @@
+package org.bukkit.metadata;
+
+@SuppressWarnings("serial")
+public class MetadataEvaluationException extends RuntimeException {
+	MetadataEvaluationException(Throwable cause) {
+		super(cause);
+	}
+}
