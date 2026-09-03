62d06c7c0081d8afafd27942b30d158e50a1cc5c
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:11:03 +0300

    Add Honami-API/src/main/java/org/bukkit/event/server/ServiceUnregisterEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/server/ServiceUnregisterEvent.java
@@ -0,0 +1,21 @@
+package org.bukkit.event.server;
+
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class ServiceUnregisterEvent extends ServiceEvent {
+	private static final HandlerList handlers = new HandlerList();
+
+	public ServiceUnregisterEvent(RegisteredServiceProvider<?> serviceProvider) {
+		super(serviceProvider);
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
