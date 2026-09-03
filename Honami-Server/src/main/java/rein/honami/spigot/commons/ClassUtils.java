dcb96e2e2184b0ccdfc5c46272c009a85a8ce381
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:30:14 +0300

    Add Honami-Server/src/main/java/rein/honami/spigot/commons/ClassUtils.java

---
+++ Honami-Server/src/main/java/rein/honami/spigot/commons/ClassUtils.java
@@ -0,0 +1,14 @@
+package rein.honami.spigot.commons;
+
+public class ClassUtils {
+
+	public static boolean exists(String className, ClassLoader loader) {
+		try {
+			Class.forName(className, true, loader);
+			return true;
+		} catch (ClassNotFoundException ignored) {
+			return false;
+		}
+	}
+
+}
