59439711a16d7c0f6d50f946a766200c7e0673d8
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:04:59 +0300

    Add Honami-API/src/main/java/org/bukkit/event/block/EntityBlockFormEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/block/EntityBlockFormEvent.java
@@ -0,0 +1,19 @@
+package org.bukkit.event.block;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.entity.Entity;
+
+public class EntityBlockFormEvent extends BlockFormEvent {
+	private final Entity entity;
+
+	public EntityBlockFormEvent(final Entity entity, final Block block, final BlockState blockstate) {
+		super(block, blockstate);
+
+		this.entity = entity;
+	}
+
+	public Entity getEntity() {
+		return entity;
+	}
+}
