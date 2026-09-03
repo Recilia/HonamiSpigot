2910ae095a2abf75b4d3def5af7cde7c36558e43
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:00:57 +0300

    Add Honami-API/src/main/java/org/bukkit/entity/Firework.java

---
+++ Honami-API/src/main/java/org/bukkit/entity/Firework.java
@@ -0,0 +1,12 @@
+package org.bukkit.entity;
+
+import org.bukkit.inventory.meta.FireworkMeta;
+
+public interface Firework extends Entity {
+
+	FireworkMeta getFireworkMeta();
+
+	void setFireworkMeta(FireworkMeta meta);
+
+	void detonate();
+}
