855b3f47da70c275c3ad3a5a989bf4719bc365d1
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:53:00 +0300

    Add Honami-API/src/main/java/org/bukkit/Rotation.java

---
+++ Honami-API/src/main/java/org/bukkit/Rotation.java
@@ -0,0 +1,30 @@
+package org.bukkit;
+
+public enum Rotation {
+
+	NONE,
+
+	CLOCKWISE_45,
+
+	CLOCKWISE,
+
+	CLOCKWISE_135,
+
+	FLIPPED,
+
+	FLIPPED_45,
+
+	COUNTER_CLOCKWISE,
+
+	COUNTER_CLOCKWISE_45;
+
+	private static final Rotation[] rotations = values();
+
+	public Rotation rotateClockwise() {
+		return rotations[(this.ordinal() + 1) & 0x7];
+	}
+
+	public Rotation rotateCounterClockwise() {
+		return rotations[(this.ordinal() - 1) & 0x7];
+	}
+}
