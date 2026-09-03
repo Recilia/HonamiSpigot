c83abd9a2b23ad5f54ae577b93e97b24bc6afe11
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:36:08 +0300

    Add net/minecraft/server/BlockMobSpawner.java

---
+++ Honami-Server/src/main/java/net/minecraft/server/BlockMobSpawner.java
@@ -0,0 +1,49 @@
+package net.minecraft.server;
+
+import java.util.Random;
+
+public class BlockMobSpawner extends BlockContainer {
+
+	protected BlockMobSpawner() {
+		super(Material.STONE);
+	}
+
+	@Override
+	public TileEntity a(World world, int i) {
+		return new TileEntityMobSpawner();
+	}
+
+	@Override
+	public Item getDropType(IBlockData iblockdata, Random random, int i) {
+		return null;
+	}
+
+	@Override
+	public int a(Random random) {
+		return 0;
+	}
+
+	@Override
+	public void dropNaturally(World world, BlockPosition blockposition, IBlockData iblockdata, float f, int i) {
+		super.dropNaturally(world, blockposition, iblockdata, f, i);
+
+	}
+
+	@Override
+	public int getExpDrop(World world, IBlockData iblockdata, int enchantmentLevel) {
+		int j = 15 + world.random.nextInt(15) + world.random.nextInt(15);
+
+		return j;
+		
+	}
+
+	@Override
+	public boolean c() {
+		return false;
+	}
+
+	@Override
+	public int b() {
+		return 3;
+	}
+}
