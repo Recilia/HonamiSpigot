2ab9e02d8c2807dd4677176652dc552d2fe86fe2
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:29:58 +0300

    Add Honami-Server/src/main/java/rein/honami/spigot/async/pathsearch/cache/SearchCacheEntryEntity.java

---
+++ Honami-Server/src/main/java/rein/honami/spigot/async/pathsearch/cache/SearchCacheEntryEntity.java
@@ -0,0 +1,19 @@
+package rein.honami.spigot.async.pathsearch.cache;
+
+import net.minecraft.server.Entity;
+import net.minecraft.server.EntityInsentient;
+import net.minecraft.server.PathEntity;
+
+public class SearchCacheEntryEntity extends SearchCacheEntry {
+	
+	private final Entity target;
+	
+	public SearchCacheEntryEntity(Entity target, EntityInsentient targetingEntity, PathEntity path) {
+		super(targetingEntity, path);
+		this.target = target;
+	}
+
+	public Entity getTarget() {
+		return target;
+	}
+}
