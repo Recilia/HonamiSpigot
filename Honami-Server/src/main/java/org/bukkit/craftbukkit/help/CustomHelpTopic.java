c310a50f0fa5020fce74abcdaff3b67d2c0bda91
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:25:10 +0300

    Add Honami-Server/src/main/java/org/bukkit/craftbukkit/help/CustomHelpTopic.java

---
+++ Honami-Server/src/main/java/org/bukkit/craftbukkit/help/CustomHelpTopic.java
@@ -0,0 +1,29 @@
+package org.bukkit.craftbukkit.help;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.help.HelpTopic;
+
+public class CustomHelpTopic extends HelpTopic {
+	private final String permissionNode;
+
+	public CustomHelpTopic(String name, String shortText, String fullText, String permissionNode) {
+		this.permissionNode = permissionNode;
+		this.name = name;
+		this.shortText = shortText;
+		this.fullText = shortText + "\n" + fullText;
+	}
+
+	@Override
+	public boolean canSee(CommandSender sender) {
+		if (sender instanceof ConsoleCommandSender) {
+			return true;
+		}
+
+		if ((permissionNode != null && !permissionNode.isEmpty())) {
+			return sender.hasPermission(permissionNode);
+		} else {
+			return true;
+		}
+	}
+}
