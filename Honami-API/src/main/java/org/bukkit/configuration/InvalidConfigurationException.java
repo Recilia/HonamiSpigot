aeacffe657621349dc82537b2d5a695025fefe89
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:57:41 +0300

    Add Honami-API/src/main/java/org/bukkit/configuration/InvalidConfigurationException.java

---
+++ Honami-API/src/main/java/org/bukkit/configuration/InvalidConfigurationException.java
@@ -0,0 +1,20 @@
+package org.bukkit.configuration;
+
+@SuppressWarnings("serial")
+public class InvalidConfigurationException extends Exception {
+
+	public InvalidConfigurationException() {
+	}
+
+	public InvalidConfigurationException(String msg) {
+		super(msg);
+	}
+
+	public InvalidConfigurationException(Throwable cause) {
+		super(cause);
+	}
+
+	public InvalidConfigurationException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
