2fc6bd12aa9f482f757068d2a045572931b71165
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:15:28 +0300

    Add Honami-API/src/main/java/org/bukkit/material/FurnaceAndDispenser.java

---
+++ Honami-API/src/main/java/org/bukkit/material/FurnaceAndDispenser.java
@@ -0,0 +1,30 @@
+package org.bukkit.material;
+
+import org.bukkit.Material;
+
+public class FurnaceAndDispenser extends DirectionalContainer {
+
+	@Deprecated
+	public FurnaceAndDispenser(final int type) {
+		super(type);
+	}
+
+	public FurnaceAndDispenser(final Material type) {
+		super(type);
+	}
+
+	@Deprecated
+	public FurnaceAndDispenser(final int type, final byte data) {
+		super(type, data);
+	}
+
+	@Deprecated
+	public FurnaceAndDispenser(final Material type, final byte data) {
+		super(type, data);
+	}
+
+	@Override
+	public FurnaceAndDispenser clone() {
+		return (FurnaceAndDispenser) super.clone();
+	}
+}
