03e2a4d19ec4ae5acf78d3afd6535c7d13506de9
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:34:52 +0300

    Add net/minecraft/server/BlockBloodStone.java

---
+++ Honami-Server/src/main/java/net/minecraft/server/BlockBloodStone.java
@@ -0,0 +1,29 @@
+package net.minecraft.server;
+
+import org.bukkit.event.block.BlockRedstoneEvent; 
+
+public class BlockBloodStone extends Block {
+
+	public BlockBloodStone() {
+		super(Material.STONE);
+		this.a(CreativeModeTab.b);
+	}
+
+	@Override
+	public MaterialMapColor g(IBlockData iblockdata) {
+		return MaterialMapColor.K;
+	}
+
+	
+	
+	public void doPhysics_nvmplsdont(World world, BlockPosition position, IBlockData iblockdata, Block block) {
+		if (block != null && block.isPowerSource()) {
+			org.bukkit.block.Block bl = world.getWorld().getBlockAt(position.getX(), position.getY(), position.getZ());
+			int power = bl.getBlockPower();
+
+			BlockRedstoneEvent event = new BlockRedstoneEvent(bl, power, power);
+			world.getServer().getPluginManager().callEvent(event);
+		}
+	}
+	
+}
