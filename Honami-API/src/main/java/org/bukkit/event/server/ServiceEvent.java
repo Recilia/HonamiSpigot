5ebe2b7f1f652540106182c2c63f0a532d741593
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:10:58 +0300

    Add Honami-API/src/main/java/org/bukkit/event/server/ServiceEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/server/ServiceEvent.java
@@ -0,0 +1,15 @@
+package org.bukkit.event.server;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public abstract class ServiceEvent extends ServerEvent {
+	private final RegisteredServiceProvider<?> provider;
+
+	public ServiceEvent(final RegisteredServiceProvider<?> provider) {
+		this.provider = provider;
+	}
+
+	public RegisteredServiceProvider<?> getProvider() {
+		return provider;
+	}
+}
