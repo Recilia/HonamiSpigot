09f8f10045788df9357aa335aed247999396e91b
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:17:17 +0300

    Add Honami-API/src/main/java/org/bukkit/metadata/MetadataConversionException.java

---
+++ Honami-API/src/main/java/org/bukkit/metadata/MetadataConversionException.java
@@ -0,0 +1,8 @@
+package org.bukkit.metadata;
+
+@SuppressWarnings("serial")
+public class MetadataConversionException extends RuntimeException {
+	MetadataConversionException(String message) {
+		super(message);
+	}
+}
