204cce7ebb61c89c1085fe47bbf74d81cfa6af4f
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:12:48 +0300

    Add Honami-API/src/main/java/org/bukkit/inventory/DoubleChestInventory.java

---
+++ Honami-API/src/main/java/org/bukkit/inventory/DoubleChestInventory.java
@@ -0,0 +1,12 @@
+package org.bukkit.inventory;
+
+import org.bukkit.block.DoubleChest;
+
+public interface DoubleChestInventory extends Inventory {
+
+	Inventory getLeftSide();
+
+	Inventory getRightSide();
+
+	DoubleChest getHolder();
+}
