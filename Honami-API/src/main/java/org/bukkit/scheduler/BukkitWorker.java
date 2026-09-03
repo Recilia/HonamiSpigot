5882de7cba323c507baa5651230a21ea9f06ef29
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:20:05 +0300

    Add Honami-API/src/main/java/org/bukkit/scheduler/BukkitWorker.java

---
+++ Honami-API/src/main/java/org/bukkit/scheduler/BukkitWorker.java
@@ -0,0 +1,13 @@
+package org.bukkit.scheduler;
+
+import org.bukkit.plugin.Plugin;
+
+public interface BukkitWorker {
+
+	public int getTaskId();
+
+	public Plugin getOwner();
+
+	public Thread getThread();
+
+}
