29938d81c16c7bc5840b185896b2bbaff195975a
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:12:33 +0300

    Add Honami-API/src/main/java/org/bukkit/help/HelpTopicFactory.java

---
+++ Honami-API/src/main/java/org/bukkit/help/HelpTopicFactory.java
@@ -0,0 +1,8 @@
+package org.bukkit.help;
+
+import org.bukkit.command.Command;
+
+public interface HelpTopicFactory<TCommand extends Command> {
+
+	public HelpTopic createTopic(TCommand command);
+}
