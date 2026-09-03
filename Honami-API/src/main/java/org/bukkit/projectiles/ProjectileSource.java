eb587141aed00b660afd4e22983d74399802e488
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:19:53 +0300

    Add Honami-API/src/main/java/org/bukkit/projectiles/ProjectileSource.java

---
+++ Honami-API/src/main/java/org/bukkit/projectiles/ProjectileSource.java
@@ -0,0 +1,11 @@
+package org.bukkit.projectiles;
+
+import org.bukkit.entity.Projectile;
+import org.bukkit.util.Vector;
+
+public interface ProjectileSource {
+
+	public <T extends Projectile> T launchProjectile(Class<? extends T> projectile);
+
+	public <T extends Projectile> T launchProjectile(Class<? extends T> projectile, Vector velocity);
+}
