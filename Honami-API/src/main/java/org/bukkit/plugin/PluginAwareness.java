5aea8cec21cef774cf15f5346d31a45a9f3ba36b
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:18:14 +0300

    Add Honami-API/src/main/java/org/bukkit/plugin/PluginAwareness.java

---
+++ Honami-API/src/main/java/org/bukkit/plugin/PluginAwareness.java
@@ -0,0 +1,13 @@
+package org.bukkit.plugin;
+
+import java.util.Set;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public interface PluginAwareness {
+
+	public enum Flags implements PluginAwareness {
+
+		UTF8,;
+	}
+}
