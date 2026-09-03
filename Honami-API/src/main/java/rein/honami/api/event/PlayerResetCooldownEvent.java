d304e6e2b5dca654357d44082703adcdaf8fdbf3
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:22:01 +0300

    Add Honami-API/src/main/java/rein/honami/api/event/PlayerResetCooldownEvent.java

---
+++ Honami-API/src/main/java/rein/honami/api/event/PlayerResetCooldownEvent.java
@@ -0,0 +1,29 @@
+package rein.honami.api.event;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerResetCooldownEvent extends Event {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final Player player;
+
+    public PlayerResetCooldownEvent(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
