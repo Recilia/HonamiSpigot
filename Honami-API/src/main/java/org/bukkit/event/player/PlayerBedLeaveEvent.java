e5b9ae8dd467574e670e51b52724027f2f45472b
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:08:34 +0300

    Add Honami-API/src/main/java/org/bukkit/event/player/PlayerBedLeaveEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/player/PlayerBedLeaveEvent.java
@@ -0,0 +1,28 @@
+package org.bukkit.event.player;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+public class PlayerBedLeaveEvent extends PlayerEvent {
+	private static final HandlerList handlers = new HandlerList();
+	private final Block bed;
+
+	public PlayerBedLeaveEvent(final Player who, final Block bed) {
+		super(who);
+		this.bed = bed;
+	}
+
+	public Block getBed() {
+		return bed;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
