6a39cb82544083dd46f9691f9718229875a68723
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:18:06 +0300

    Add Honami-API/src/main/java/org/bukkit/plugin/InvalidDescriptionException.java

---
+++ Honami-API/src/main/java/org/bukkit/plugin/InvalidDescriptionException.java
@@ -0,0 +1,21 @@
+package org.bukkit.plugin;
+
+public class InvalidDescriptionException extends Exception {
+	private static final long serialVersionUID = 5721389122281775896L;
+
+	public InvalidDescriptionException(final Throwable cause, final String message) {
+		super(message, cause);
+	}
+
+	public InvalidDescriptionException(final Throwable cause) {
+		super("Invalid plugin.yml", cause);
+	}
+
+	public InvalidDescriptionException(final String message) {
+		super(message);
+	}
+
+	public InvalidDescriptionException() {
+		super("Invalid plugin.yml");
+	}
+}
