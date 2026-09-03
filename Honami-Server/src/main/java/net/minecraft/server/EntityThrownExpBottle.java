285fb945ff5c35a85a10a66b1f3a4d03bdb15d28
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:42:03 +0300

    Add net/minecraft/server/EntityThrownExpBottle.java

---
+++ Honami-Server/src/main/java/net/minecraft/server/EntityThrownExpBottle.java
@@ -0,0 +1,56 @@
+package net.minecraft.server;
+
+public class EntityThrownExpBottle extends EntityProjectile {
+
+	public EntityThrownExpBottle(World world) {
+		super(world);
+	}
+
+	public EntityThrownExpBottle(World world, EntityLiving entityliving) {
+		super(world, entityliving);
+	}
+
+	public EntityThrownExpBottle(World world, double d0, double d1, double d2) {
+		super(world, d0, d1, d2);
+	}
+
+	@Override
+	protected float m() {
+		return 0.07F;
+	}
+
+	@Override
+	protected float j() {
+		return 0.7F;
+	}
+
+	@Override
+	protected float l() {
+		return -20.0F;
+	}
+
+	@Override
+	protected void a(MovingObjectPosition movingobjectposition) {
+		if (!this.world.isClientSide) {
+
+			int i = 3 + this.world.random.nextInt(5) + this.world.random.nextInt(5);
+
+			org.bukkit.event.entity.ExpBottleEvent event = org.bukkit.craftbukkit.event.CraftEventFactory
+					.callExpBottleEvent(this, i);
+			i = event.getExperience();
+			if (event.getShowEffect()) {
+				this.world.triggerEffect(2002, new BlockPosition(this), 0);
+			}
+
+			while (i > 0) {
+				int j = EntityExperienceOrb.getOrbValue(i);
+
+				i -= j;
+				this.world.addEntity(new EntityExperienceOrb(this.world, this.locX, this.locY, this.locZ, j));
+			}
+
+			this.die();
+		}
+
+	}
+}
