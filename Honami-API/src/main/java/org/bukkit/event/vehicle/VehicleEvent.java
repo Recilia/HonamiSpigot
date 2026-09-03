4553a03dbf61be9406be6b3b93053fee6e2b2084
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:11:24 +0300

    Add Honami-API/src/main/java/org/bukkit/event/vehicle/VehicleEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/vehicle/VehicleEvent.java
@@ -0,0 +1,16 @@
+package org.bukkit.event.vehicle;
+
+import org.bukkit.entity.Vehicle;
+import org.bukkit.event.Event;
+
+public abstract class VehicleEvent extends Event {
+	protected Vehicle vehicle;
+
+	public VehicleEvent(final Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public final Vehicle getVehicle() {
+		return vehicle;
+	}
+}
