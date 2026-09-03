b78ce80e0a50be260560a5be46589a03bcae69af
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:46:31 +0300

    Add net/minecraft/server/PlayerConnectionUtils.java

---
+++ Honami-Server/src/main/java/net/minecraft/server/PlayerConnectionUtils.java
@@ -0,0 +1,16 @@
+package net.minecraft.server;
+
+public class PlayerConnectionUtils {
+    
+	public static <T extends PacketListener> void ensureMainThread(final Packet<T> packet, final T listener, WorldServer worldServer) throws CancelledPacketHandleException {
+        ensureMainThread(packet, listener, worldServer.getMinecraftServer());
+    }
+
+    public static <T extends PacketListener> void ensureMainThread(final Packet<T> packet, final T listener, IAsyncTaskHandler<?> handler) throws CancelledPacketHandleException {
+    
+		if (!packet.instant() && !handler.isMainThread()) {
+			handler.postToMainThread(() -> packet.a(listener));
+			throw CancelledPacketHandleException.INSTANCE;
+		}
+	}
+}
