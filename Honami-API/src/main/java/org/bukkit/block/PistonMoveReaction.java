e0a0c31f42d509afe5f61b469d98e64c6ceb11cc
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:54:31 +0300

    Add Honami-API/src/main/java/org/bukkit/block/PistonMoveReaction.java

---
+++ Honami-API/src/main/java/org/bukkit/block/PistonMoveReaction.java
@@ -0,0 +1,35 @@
+package org.bukkit.block;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum PistonMoveReaction {
+
+	MOVE(0),
+
+	BREAK(1),
+
+	BLOCK(2);
+
+	private int id;
+	private static Map<Integer, PistonMoveReaction> byId = new HashMap<Integer, PistonMoveReaction>();
+	static {
+		for (PistonMoveReaction reaction : PistonMoveReaction.values()) {
+			byId.put(reaction.id, reaction);
+		}
+	}
+
+	private PistonMoveReaction(int id) {
+		this.id = id;
+	}
+
+	@Deprecated
+	public int getId() {
+		return this.id;
+	}
+
+	@Deprecated
+	public static PistonMoveReaction getById(int id) {
+		return byId.get(id);
+	}
+}
