a5da7dcd79e828fb7b8b4ff0f5fcb5b1dccd7010
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:09:18 +0300

    Add Honami-API/src/main/java/org/bukkit/event/player/PlayerInteractAtEntityEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/player/PlayerInteractAtEntityEvent.java
@@ -0,0 +1,29 @@
+package org.bukkit.event.player;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.util.Vector;
+
+public class PlayerInteractAtEntityEvent extends PlayerInteractEntityEvent {
+	private static final HandlerList handlers = new HandlerList();
+	private final Vector position;
+
+	public PlayerInteractAtEntityEvent(Player who, Entity clickedEntity, Vector position) {
+		super(who, clickedEntity);
+		this.position = position;
+	}
+
+	public Vector getClickedPosition() {
+		return position.clone();
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
