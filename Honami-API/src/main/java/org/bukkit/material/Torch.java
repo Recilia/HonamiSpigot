d4eb54d9c54b3651d2ef002a0eb087fe9c844072
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:16:49 +0300

    Add Honami-API/src/main/java/org/bukkit/material/Torch.java

---
+++ Honami-API/src/main/java/org/bukkit/material/Torch.java
@@ -0,0 +1,84 @@
+package org.bukkit.material;
+
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+
+public class Torch extends SimpleAttachableMaterialData {
+	public Torch() {
+		super(Material.TORCH);
+	}
+
+	@Deprecated
+	public Torch(final int type) {
+		super(type);
+	}
+
+	public Torch(final Material type) {
+		super(type);
+	}
+
+	@Deprecated
+	public Torch(final int type, final byte data) {
+		super(type, data);
+	}
+
+	@Deprecated
+	public Torch(final Material type, final byte data) {
+		super(type, data);
+	}
+
+	public BlockFace getAttachedFace() {
+		byte data = getData();
+
+		switch (data) {
+		case 0x1:
+			return BlockFace.WEST;
+
+		case 0x2:
+			return BlockFace.EAST;
+
+		case 0x3:
+			return BlockFace.NORTH;
+
+		case 0x4:
+			return BlockFace.SOUTH;
+
+		case 0x5:
+		default:
+			return BlockFace.DOWN;
+		}
+	}
+
+	public void setFacingDirection(BlockFace face) {
+		byte data;
+
+		switch (face) {
+		case EAST:
+			data = 0x1;
+			break;
+
+		case WEST:
+			data = 0x2;
+			break;
+
+		case SOUTH:
+			data = 0x3;
+			break;
+
+		case NORTH:
+			data = 0x4;
+			break;
+
+		case UP:
+		default:
+			data = 0x5;
+		}
+
+		setData(data);
+	}
+
+	@Override
+	public Torch clone() {
+		return (Torch) super.clone();
+	}
+}
