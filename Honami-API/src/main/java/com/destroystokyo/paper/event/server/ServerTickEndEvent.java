980e182dc988b77c6a110f8d0e15250c9d6b0e1d
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:32:24 +0300

    Add Honami-API/src/main/java/com/destroystokyo/paper/event/server/ServerTickEndEvent.java

---
+++ Honami-API/src/main/java/com/destroystokyo/paper/event/server/ServerTickEndEvent.java
@@ -0,0 +1,38 @@
+package com.destroystokyo.paper.event.server;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class ServerTickEndEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+	private final int tickNumber;
+	private final double tickDuration;
+	private final long timeEnd;
+
+	public ServerTickEndEvent(int tickNumber, double tickDuration, long timeRemaining) {
+		this.tickNumber = tickNumber;
+		this.tickDuration = tickDuration;
+		this.timeEnd = System.nanoTime() + timeRemaining;
+	}
+
+	public int getTickNumber() {
+		return tickNumber;
+	}
+
+	public double getTickDuration() {
+		return tickDuration;
+	}
+
+	public long getTimeRemaining() {
+		return this.timeEnd - System.nanoTime();
+	}
+
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
