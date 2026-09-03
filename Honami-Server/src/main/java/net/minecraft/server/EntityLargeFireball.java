947b779e6c2f4e161769ccda18ac77cbd505c401
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:40:57 +0300

    Add net/minecraft/server/EntityLargeFireball.java

---
+++ Honami-Server/src/main/java/net/minecraft/server/EntityLargeFireball.java
@@ -0,0 +1,58 @@
+package net.minecraft.server;
+
+import org.bukkit.event.entity.ExplosionPrimeEvent; 
+
+public class EntityLargeFireball extends EntityFireball {
+
+	public int yield = 1;
+
+	public EntityLargeFireball(World world) {
+		super(world);
+	}
+
+	public EntityLargeFireball(World world, EntityLiving entityliving, double d0, double d1, double d2) {
+		super(world, entityliving, d0, d1, d2);
+	}
+
+	@Override
+	protected void a(MovingObjectPosition movingobjectposition) {
+		if (!this.world.isClientSide) {
+			if (movingobjectposition.entity != null) {
+				movingobjectposition.entity.damageEntity(DamageSource.fireball(this, this.shooter), 6.0F);
+				this.a(this.shooter, movingobjectposition.entity);
+			}
+
+			boolean flag = this.world.getGameRules().getBoolean("mobGriefing");
+
+			ExplosionPrimeEvent event = new ExplosionPrimeEvent(
+					(org.bukkit.entity.Explosive) org.bukkit.craftbukkit.entity.CraftEntity
+							.getEntity(this.world.getServer(), this));
+			this.world.getServer().getPluginManager().callEvent(event);
+
+			if (!event.isCancelled()) {
+				
+				this.world.createExplosion(this, this.locX, this.locY, this.locZ, event.getRadius(), event.getFire(),
+						flag);
+			}
+			
+			this.die();
+		}
+
+	}
+
+	@Override
+	public void b(NBTTagCompound nbttagcompound) {
+		super.b(nbttagcompound);
+		nbttagcompound.setInt("ExplosionPower", this.yield);
+	}
+
+	@Override
+	public void a(NBTTagCompound nbttagcompound) {
+		super.a(nbttagcompound);
+		if (nbttagcompound.hasKeyOfType("ExplosionPower", 99)) {
+			
+			bukkitYield = this.yield = nbttagcompound.getInt("ExplosionPower");
+		}
+
+	}
+}
