3556b926f9b51d09586eb003ad3c122f9ffaa507
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:29:40 +0300

    Add Honami-Server/src/main/java/rein/honami/spigot/async/ResettableLatch.java

---
+++ Honami-Server/src/main/java/rein/honami/spigot/async/ResettableLatch.java
@@ -0,0 +1,42 @@
+package rein.honami.spigot.async;
+
+import javafixes.concurrency.ReusableCountLatch;
+
+public class ResettableLatch extends ReusableCountLatch {
+
+	private int initValue;
+
+	public ResettableLatch() {
+		this(0);
+	}
+
+	public ResettableLatch(int initialCount) {
+		super(initialCount);
+		this.initValue = initialCount;
+	}
+
+	public void reset() {
+		reset(initValue);
+	}
+
+	public void reset(int count) {
+		
+		if (getCount() > count) {
+
+			while (getCount() > count) {
+				
+				decrement();
+			}
+
+		} else if (getCount() < count) {
+
+			while (getCount() < count) {
+				
+				increment();
+			}
+
+		}
+		
+	}
+
+}
