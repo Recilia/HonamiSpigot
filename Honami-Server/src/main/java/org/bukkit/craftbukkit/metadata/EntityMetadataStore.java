9f1545e7443275f016f3186a4d57191e618b4afa
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:27:14 +0300

    Add Honami-Server/src/main/java/org/bukkit/craftbukkit/metadata/EntityMetadataStore.java

---
+++ Honami-Server/src/main/java/org/bukkit/craftbukkit/metadata/EntityMetadataStore.java
@@ -0,0 +1,13 @@
+package org.bukkit.craftbukkit.metadata;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.metadata.MetadataStore;
+import org.bukkit.metadata.MetadataStoreBase;
+
+public class EntityMetadataStore extends MetadataStoreBase<Entity> implements MetadataStore<Entity> {
+
+	@Override
+	protected String disambiguate(Entity entity, String metadataKey) {
+		return entity.getUniqueId().toString() + ":" + metadataKey;
+	}
+}
