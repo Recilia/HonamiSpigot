4e88c68160e68ed2915e248a729d1bf5a7bc0e8d
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:07:01 +0300

    Add Honami-API/src/main/java/org/bukkit/event/entity/SheepRegrowWoolEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/entity/SheepRegrowWoolEvent.java
@@ -0,0 +1,38 @@
+package org.bukkit.event.entity;
+
+import org.bukkit.entity.Sheep;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class SheepRegrowWoolEvent extends EntityEvent implements Cancellable {
+	private static final HandlerList handlers = new HandlerList();
+	private boolean cancel;
+
+	public SheepRegrowWoolEvent(final Sheep sheep) {
+		super(sheep);
+		this.cancel = false;
+	}
+
+	public boolean isCancelled() {
+		return cancel;
+	}
+
+	public void setCancelled(boolean cancel) {
+		this.cancel = cancel;
+	}
+
+	@Override
+	public Sheep getEntity() {
+		return (Sheep) entity;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+}
