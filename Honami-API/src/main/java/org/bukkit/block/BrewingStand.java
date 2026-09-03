d7b09cf70b8c0225133e66f903d6b1f64d87a43e
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 17:54:00 +0300

    Add Honami-API/src/main/java/org/bukkit/block/BrewingStand.java

---
+++ Honami-API/src/main/java/org/bukkit/block/BrewingStand.java
@@ -0,0 +1,12 @@
+package org.bukkit.block;
+
+import org.bukkit.inventory.BrewerInventory;
+
+public interface BrewingStand extends BlockState, ContainerBlock {
+
+	int getBrewingTime();
+
+	void setBrewingTime(int brewTime);
+
+	public BrewerInventory getInventory();
+}
