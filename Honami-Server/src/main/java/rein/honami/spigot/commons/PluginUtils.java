d03134d568ddd68658b63d1bb284e56f1a48854c
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:30:24 +0300

    Add Honami-Server/src/main/java/rein/honami/spigot/commons/PluginUtils.java

---
+++ Honami-Server/src/main/java/rein/honami/spigot/commons/PluginUtils.java
@@ -0,0 +1,12 @@
+package rein.honami.spigot.commons;
+import org.bukkit.plugin.Plugin;
+
+public class PluginUtils {
+    public static int getCitizensBuild(Plugin plugin) {
+        try {
+            return Integer.parseInt(plugin.getDescription().getVersion().split("\\(build ")[1].replace(")", ""));
+        } catch (Throwable ignored) {
+            return 2396;
+        }
+    }
+}
