e2567cb30030be0feb3bef06d95f2ac0a9638bfd
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:25:23 +0300

    Add Honami-Server/src/main/java/org/bukkit/craftbukkit/help/MultipleCommandAliasHelpTopicFactory.java

---
+++ Honami-Server/src/main/java/org/bukkit/craftbukkit/help/MultipleCommandAliasHelpTopicFactory.java
@@ -0,0 +1,13 @@
+package org.bukkit.craftbukkit.help;
+
+import org.bukkit.command.MultipleCommandAlias;
+import org.bukkit.help.HelpTopic;
+import org.bukkit.help.HelpTopicFactory;
+
+public class MultipleCommandAliasHelpTopicFactory implements HelpTopicFactory<MultipleCommandAlias> {
+
+	@Override
+	public HelpTopic createTopic(MultipleCommandAlias multipleCommandAlias) {
+		return new MultipleCommandAliasHelpTopic(multipleCommandAlias);
+	}
+}
