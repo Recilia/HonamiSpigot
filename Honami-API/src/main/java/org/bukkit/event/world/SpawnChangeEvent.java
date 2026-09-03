29a4b93652df8dc8ffb1a05f8987c1ce88e615a8
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:11:58 +0300

    Add Honami-API/src/main/java/org/bukkit/event/world/SpawnChangeEvent.java

---
+++ Honami-API/src/main/java/org/bukkit/event/world/SpawnChangeEvent.java
@@ -0,0 +1,28 @@
+package org.bukkit.event.world;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.event.HandlerList;
+
+public class SpawnChangeEvent extends WorldEvent {
+	private static final HandlerList handlers = new HandlerList();
+	private final Location previousLocation;
+
+	public SpawnChangeEvent(final World world, final Location previousLocation) {
+		super(world);
+		this.previousLocation = previousLocation;
+	}
+
+	public Location getPreviousLocation() {
+		return previousLocation;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
