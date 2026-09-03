a9c84790c5afa278af246042068ec7cf4153d560
⠀ <75722308+Recilia@users.noreply.github.com>
2026-09-03 18:27:17 +0300

    Add Honami-Server/src/main/java/org/bukkit/craftbukkit/metadata/PlayerMetadataStore.java

---
+++ Honami-Server/src/main/java/org/bukkit/craftbukkit/metadata/PlayerMetadataStore.java
@@ -0,0 +1,13 @@
+package org.bukkit.craftbukkit.metadata;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.metadata.MetadataStore;
+import org.bukkit.metadata.MetadataStoreBase;
+
+public class PlayerMetadataStore extends MetadataStoreBase<OfflinePlayer> implements MetadataStore<OfflinePlayer> {
+
+	@Override
+	protected String disambiguate(OfflinePlayer player, String metadataKey) {
+		return player.getName().toLowerCase() + ":" + metadataKey;
+	}
+}
